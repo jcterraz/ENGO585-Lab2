d09af9071bbeda2826d001c26a317311948e6411
Juan Carlos Terrazas Borbon <jcterraz@ucalgary.ca>
2018-01-26 16:39:54 -0700

    Start Coding

---
+++ Main.m
@@ -0,0 +1,26 @@
+% Author: Juan Carlos Terrazas Borbon
+% Last Update: 2018-01-26
+% Course: ENGO 585
+% Lab: 2
+
+% ---------------------Purpose of Code-----------------------------------
+% The purpose of this code is to able to perform the 3 task required to
+% perfome in the lab handout which involves Least Squares adjustment and
+% Kalman filter
+
+% Clear variables, close figure and command
+clc
+clear all
+close all
+
+% Read the file with data
+data = load('Lab2data.txt');
+
+%% Task 1: Batch Parametric Least Squares
+
+
+%% Task 2: Summation of Normals and Sequential LS
+
+
+%% Task 3: Kalman Filtering
+
